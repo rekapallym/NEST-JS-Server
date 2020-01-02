8f7df5ae0c6f8939aacd493e0fef81eff1958c9f
first last name <me@email.com>
2020-01-02 15:20:08 -0500

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 import { BigtableService } from './bigtable/bigtable.service';
+import { TransLogService } from './translog.service';
 // import { BigtableController } from './bigtable.controller';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [BigtableService],
+  providers: [TransLogService],
 })
 export class AppModule {}
